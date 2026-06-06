360a50988993e0920de86bcd29e939b8a045e9a0
jtin46 <godejatin293@gmail.com>
2026-06-06 19:16:02 +0530

    ch

--- server.ts
+++ server.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import "dotenv/config";import "dotenv/config";
+import "dotenv/config";
 import path from "path";
 import dns from "dns";
 import { createServer as createViteServer } from "vite";
